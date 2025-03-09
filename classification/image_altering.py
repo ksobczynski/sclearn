d005f26adb005777911abfb766394140c7c2e6c9
Konstanty Sobczynski <01199334@pw.edu.pl>
2025-03-09 12:58:46 +0100

    poprawka poprzedniego comm

--- classification/image_altering.py
+++ classification/image_altering.py
@@ -1,7 +1,32 @@
 from sklearn.datasets import fetch_openml
+import numpy as np
 mnist = fetch_openml('mnist_784', as_frame=False)
-
-print(mnist.data[0])
-
-def move_image():
-    return
+X = mnist.data
+def move_image(dir, img):
+    match dir:
+        case "up":
+            row1 = img[:28]
+            img = img[28:]
+            img = np.concatenate((img, row1))
+            # img = img + dir
+            # for i in range(28):
+            #     img[i], img[755+i] = img[755]+i, img[i+i]
+        case "down":
+            row28 = img[755:]
+            img = img[:755]
+            img = np.concatenate((row28, img))
+            # img = row28 + img
+        case "right":
+            for i in range(28):
+                last_pxl = img[i*28+27]
+                img.pop(i*28+27)
+                img.insert(i*28)
+        case "left":
+            for i in range(28):
+                first_pcl = img[i*28]
+                img.pop(i*28)
+                img.insert(i*28+27)
+    return img
+print(X[0][84+28+28:84+28+28+28])
+W = move_image("up", X[0])
+print(W[84+28:84+28+28])
