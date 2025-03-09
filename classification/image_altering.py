4d2cf64ceed3f1d7e16c44651cf7e053d6a3358c
Konstanty Sobczynski <01199334@pw.edu.pl>
2025-03-09 20:55:38 +0100

    Created a function that augments the data array by one pixel in given direction, added .gitignore, extended the dataset and improved precision by 0,5%'

--- classification/image_altering.py
+++ classification/image_altering.py
@@ -5,28 +5,21 @@ X = mnist.data
 def move_image(dir, img):
     match dir:
         case "up":
-            row1 = img[:28]
+            # row1 = img[:28]
             img = img[28:]
-            img = np.concatenate((img, row1))
-            # img = img + dir
-            # for i in range(28):
-            #     img[i], img[755+i] = img[755]+i, img[i+i]
+            img = np.concatenate((img, np.zeros(28)))
         case "down":
-            row28 = img[755:]
-            img = img[:755]
-            img = np.concatenate((row28, img))
-            # img = row28 + img
+            # row28 = img[756:]
+            img = img[:756]
+            img = np.concatenate((np.zeros(28), img))
         case "right":
             for i in range(28):
-                last_pxl = img[i*28+27]
-                img.pop(i*28+27)
-                img.insert(i*28)
+                # last_pxl = img[i*28+27]
+                img = np.delete(img,i*28+27)
+                img = np.insert(img,i*28, 0)
         case "left":
             for i in range(28):
-                first_pcl = img[i*28]
-                img.pop(i*28)
-                img.insert(i*28+27)
+                # first_pcl = img[i*28]
+                img = np.delete(img,i*28)
+                img = np.insert(img,i*28+27, 0)
     return img
-print(X[0][84+28+28:84+28+28+28])
-W = move_image("up", X[0])
-print(W[84+28:84+28+28])
